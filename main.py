cc704504271589b2fcc2ec1f29b62656b96b5528
Felipe Quiros <feldevcode@gmail.com>
2021-11-22 16:24:04 -0500

    defaultdict and counter added

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import pprint
+from collections import defaultdict
 
 def main():
     f = open('text.txt')
     # pprint.pprint(f)
     s = f.read()
-    pprint.pprint(s)
-    print(type(s))
+    # pprint.pprint(s)
+    # print(type(s))
+    # wordList = s.split()
+    # pprint.pprint(wordList)
+    # print(type(wordList))
+    wordList = s.split()
+    position = 0
+    myDict = defaultdict(int)
+    while position < len(wordList)-2:
+        myDict[(wordList[position],wordList[position+1],wordList[position+2])] += 1
+        # print(wordList[position:position+3])
+        position +=1
+    pprint.pprint(myDict)
 if __name__ == "__main__":
     main()
