d2577d66a3748af2dd41f8126a78838b53f2d940
Felipe Quiros <feldevcode@gmail.com>
2021-12-01 14:04:33 -0500

    sys.argv implemented

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import pprint
 from collections import defaultdict
+import sys
 
 def main():
-    f = open('text.txt')
-    # pprint.pprint(f)
+    if len(sys.argv) != 2:
+        print('Usage: python main.py <path to file>')
+        sys.exit(1)
+        
+    textFile = sys.argv[1]
+    f = open(textFile)
     s = f.read()
-    # pprint.pprint(s)
-    # print(type(s))
-    # wordList = s.split()
-    # pprint.pprint(wordList)
-    # print(type(wordList))
     wordList = s.split()
     position = 0
+    topN = 100
     myDict = defaultdict(int)
     while position < len(wordList)-2:
         myDict[(wordList[position],wordList[position+1],wordList[position+2])] += 1
-        # print(wordList[position:position+3])
         position +=1
-    pprint.pprint(myDict)
+    for w in sorted(myDict, key=myDict.get, reverse=True)[:topN]:
+        print(w, myDict[w])
+        # continue
+    
 if __name__ == "__main__":
     main()
