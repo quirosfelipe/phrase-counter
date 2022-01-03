30456853afa17c737d738cb8a06f5e75b6f0575d
Felipe Quiros <feldevcode@gmail.com>
2022-01-03 17:03:55 -0500

    improvements

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-import pprint
 from collections import defaultdict
 import sys
 
+
 def main():
     if len(sys.argv) != 2:
-        print('Usage: python main.py <path to file>')
+        print("Usage: python main.py <path to file>")
         sys.exit(1)
-        
-    textFile = sys.argv[1]
-    f = open(textFile)
+
+    text_file = sys.argv[1]
+    f = open(text_file)
     s = f.read()
     wordList = s.split()
     position = 0
     topN = 100
     myDict = defaultdict(int)
-    while position < len(wordList)-2:
-        myDict[(wordList[position],wordList[position+1],wordList[position+2])] += 1
-        position +=1
+    while position < len(wordList) - 2:
+        word_one = wordList[position].lower()
+        word_two = wordList[position + 1].lower()
+        word_three = wordList[position + 2].lower()
+        myDict[
+            (word_one, word_two, word_three)
+        ] += 1
+        position += 1
     for w in sorted(myDict, key=myDict.get, reverse=True)[:topN]:
         print(w, myDict[w])
-        # continue
-    
+
+
 if __name__ == "__main__":
     main()
